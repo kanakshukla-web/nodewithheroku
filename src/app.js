bbf68ec2e52f79b8bd9687818e8a42c510056d04
Kanak <shukla.kanak7@gmail.com>
2020-12-09 17:10:20 +0530

    partials and views in express

--- src/app.js
+++ src/app.js
@@ -1,28 +1,34 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const hbs = require("hbs");
 
 const port = process.env.PORT || 3000;
 const staticPath = path.join(__dirname, "../public");
+const templatePath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 //middleware
 app.use(express.static(staticPath));
-app.use(express.static(path.join(__dirname,'../images')));
+app.use(express.static(path.join(__dirname, "../images")));
 
 // set view engine
 app.set("view engine", "hbs");
+app.set("views", templatePath);
+hbs.registerPartials(partialsPath);
 
 //template engine route
-app.get("/contact", (req, res) => {
+app.get("/login", (req, res) => {
   console.log("Welcome to the Contact");
-  res.render("index", {
+  res.render("login", {
     name: "Kanak",
   });
 });
 
 app.get("/", (req, res) => {
   console.log("Welcome to the Home");
-  res.send("Welcome to the home");
+  //res.render("index");
+  res.send("Welcome to Home")
 });
 
 app.listen(port, () => {
